1ed7a7b3747ed04cbe153e2503e679d29026e92e
StychukMaria <stychuk.maria@gmail.com>
2026-08-26 20:56:44 +0300

    fix bugs4

--- cinema/views.py
+++ cinema/views.py
@@ -111,16 +111,11 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
         if self.action in "retrieve":
             queryset = queryset.select_related("movie", "cinema_hall")
         elif self.action in "list":
-            queryset = (
-                queryset
-                .select_related("movie", "cinema_hall")
-                .annotate(
-                    tickets_available=(
-                        F("cinema_hall__rows") * F("cinema_hall__seats_in_row")
-                        - Count("tickets")
-                    )
-                )
-            ).order_by("id")
+            tickets_count = (
+                F("cinema_hall__rows") * F("cinema_hall__seats_in_row")
+                - Count("tickets")
+            )
+            queryset = queryset.annotate(tickets_available=tickets_count)
 
         return queryset.distinct()
 
